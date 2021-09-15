fff34ac8a33701d1989995b7f685959980eb4101
gunarp <gunarp@cs.washington.edu>
2021-09-15 11:00:11 -0700

    add gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,18 @@ module.exports = {
     "gatsby-plugin-styled-components",
     "gatsby-plugin-react-helmet",
     {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: [
+          "G-3N9F0EF42K",
+        ],
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+        },
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
